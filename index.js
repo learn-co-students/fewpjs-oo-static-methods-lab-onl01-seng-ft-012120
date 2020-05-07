712619d61f254f97df94a01cfed533f7ae9d5e5b
EC2 Default User <ec2-user@ip-172-31-36-178.us-east-2.compute.internal>
2020-05-07 22:49:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+      return str[0].toUpperCase() + str.slice(1)
+     
+  }
+  
+  static sanitize(string) {
+       return string.replace(/[^A-Za-z0-9\s-']+/g, '');
+  }
+  
+  static titleize(str) {
+   let arr =  str.split(" ") 
+   let result = []
+   const exclude = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+   for (let i = 0; i < arr.length; i++) {
+       if (i === 0) {
+           result.push(this.capitalize(arr[i]))
+       }else {
+           if (exclude.includes(arr[i])) {
+               result.push(arr[i])
+           }else {
+            result.push(Formatter.capitalize(arr[i]))
+           }
+       }
+   }
+   return result.join(" ")
+  }
+  
 }
