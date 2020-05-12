bc7c1f30855e0f8500ca33032d571642c1c6338f
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-12 05:06:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
   //add static methods here
+  
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  
+  static sanitize(str) {
+    return str.replace( /[^A-Za-z0-9 '-]/g, '' );
+  }
+  
+  static titleize(str) {
+    let wordsToTitlize = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    
+    let splitStr = str.split(" ");
+    let result = [];
+    
+    for (let i = 0; i < splitStr.length; i++) {
+      if (i === 0) {
+        result.push(this.capitalize(splitStr[i]));
+      }
+      
+      else {
+        if (wordsToTitlize.includes(splitStr[i])) {
+          result.push(splitStr[i]);
+        } 
+        
+        else {
+          result.push(this.capitalize(splitStr[i]));
+        }
+      }
+    }
+    return result.join(" ");
+  }
 }
