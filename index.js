16aa3569dd3f9393a55faf2a681f1e92e32aac5b
xdundun <xdundun@gmail.com>
2020-05-19 21:53:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  static sanitize(word) {
+    return word.replace(/[^A-Za-z0-9- '-]+/g, '');
+  }
+
+  static titleize(sentence) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = []
+    let splitSentence = sentence.split(" ")
+    for(let i = 0; i < splitSentence.length; i++) {
+      if(i == 0) {
+        result.push(this.capitalize(splitSentence[i]))
+      } else {
+        if(exceptions.includes(splitSentence[i])) {
+          result.push(splitSentence[i])
+        } else {
+          result.push(this.capitalize(splitSentence[i]))
+        }
+      }
+    }
+    return result.join(" ")
+  }
 }
+
